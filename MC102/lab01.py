7fddda9c5353a7a984311b55c44d7801996a5a5a
Felipe Camilo <70548685+felipecamilo@users.noreply.github.com>
2022-10-05 05:31:41 -0300

    labs 1-4

---
+++ MC102/lab01.py
@@ -0,0 +1,19 @@
+###################################################
+# MC102 - Algoritmos e Programação de Computadores
+# Laboratório 1 - Aritmética com Inteiros
+# Nome: Felipe Camilo de Queiroz
+# RA: 222006
+###################################################
+
+a = int(input())
+b = int(input())
+
+print("a =", a)
+print("b =", b)
+print("a + b =", a + b)
+print("a - b =", a - b)
+print("a * b =", a * b)
+print("a ** b =", a ** b)
+print("a // b =", a // b)
+print("a % b =", a % b)
+
