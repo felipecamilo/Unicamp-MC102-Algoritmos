9888769936463149f700115bff04aa06852dc9f5
Felipe Camilo <70548685+felipecamilo@users.noreply.github.com>
2022-10-14 23:42:07 -0300

    Labs 1-6

---
+++ MC102/lab05.py
@@ -0,0 +1,48 @@
+##################################################
+# MC102 - Algoritmos e Programação de Computadores
+# Laboratório 5 - Jornada de Trabalho
+# Nome:Felpe Camilo de Queiroz
+# RA:222006
+##################################################
+
+# Leitura do valor da hora
+V = int(input())
+
+# Leitura do numero de dias trabalhados na semana
+D = int(input())
+
+# Leitora e processamento dos periodos de trabalho de cada dia
+
+HTTotal = 0
+horas_extras = 0
+HTDia=0
+
+for i in range(0,D):
+    P = int(input())
+    for item in range(0,P):
+        HI = int(input())
+        HF = int(input())
+        HTPeriodo = HF - HI
+        HTDia += HTPeriodo
+        HTTotal += HTPeriodo
+        
+    if HTDia > 8:
+        horas_extras += HTDia-8
+    HTDia =0
+        
+        
+Horas_comuns = HTTotal-horas_extras
+        
+# Calculo do valor devido ao funcionário
+
+
+if Horas_comuns > 44:
+    horas_extras += Horas_comuns-44
+
+valor = (V * HTTotal) + (V/2 * horas_extras) 
+
+
+# Impressão da saída
+print("Horas trabalhadas:", HTTotal)
+print("Horas extras:", horas_extras)
+print("Valor devido: R$ {:0.2f}".format(valor))
---
+++ MC102/lab06.py
@@ -0,0 +1,32 @@
+##################################################
+# MC102 - Algoritmos e Programação de Computadores
+# Laboratório 6 - Torre de Panquecas
+# Nome: Felipe Camilo de Queiroz
+# RA: 222006
+##################################################
+
+# Leitura da torre de panquecas
+
+torre = [int(panqueca) for panqueca in input().split()]
+
+# Leitura e processamento dos movimentos
+
+M = None
+torreFinal = []
+
+while M != 0:
+    M = int(input())
+    if M!=0:
+        torretrocada=torre[M-1::-1]
+        for i in range(0, M):
+            torre[i]=torretrocada[i]
+    
+
+# Impressão da saída
+
+ordemCorreta = sorted(torre)
+
+if torre == ordemCorreta:
+    print('Torre estavel')
+else:
+    print('Torre instavel')
